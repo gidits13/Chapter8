3544f768ede41c5e581291467ae603f00ecc0e42
gidits13 <gidits13@gmail.com>
2022-08-28 15:21:59 +0300

    Добавьте файлы проекта.

---
+++ Chapter8/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryManager
+
+{
+    class Program
+    {
+        static void GetFilesCount()
+        {
+            string dirName = @"/";
+            if (Directory.Exists(dirName))
+            {
+                string[] files = Directory.GetFiles(dirName);
+                Console.WriteLine("Количество файлов в директории {0}: {1}", dirName, files.Length);
+            }
+
+        }
+        static void GetCatalogs()
+        {
+            string dirName = @"/"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+            if (Directory.Exists(dirName)) // Проверим, что директория существует
+            {
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+
+                foreach (string d in dirs) // Выведем их все
+                    Console.WriteLine(d);
+
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+
+                foreach (string s in files)   // Выведем их все
+                    Console.WriteLine(s);
+            }
+        }
+            
+            static void Main(string[] args)
+        { 
+            // получим системные диски
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            // Пробежимся по дискам и выведем их свойства
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем: {drive.TotalSize}");
+                    Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                }
+            }
+
+            GetCatalogs(); //   Вызов метода получения директорий
+            GetFilesCount();
+        }
+
+    }
+}
